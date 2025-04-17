8b2ccaedfec3d869b5a56ff40f39640190751379
elliotirving <elliotirving44@hotmail.com>
2025-04-17 12:10:01 +0000

    Generalized transform functions. Confirmed working.

--- src/3dgs/demo_lidar.py
+++ src/3dgs/demo_lidar.py
@@ -9,54 +9,59 @@ from PIL import Image
 import torchvision
 import time
 
-def fastlivo_to_gs_extrinsic(tx, ty, tz, qx, qy, qz, qw):
-    """
-    Convert FASTLIVO coordinate system pose (tx, ty, tz, qx, qy, qz, qw) to extrinsic in GS frame.
-    From camera's POV:
-    - FASTLIVO coordinate system: x-forward, y-left, z-up
-    - GS / Lietorch: x-right, y-down, z-forward
-    """
-    def quaternion_to_rotation_matrix(qx, qy, qz, qw):
-        norm = np.sqrt(qx*qx + qy*qy + qz*qz + qw*qw)
-        qx, qy, qz, qw = qx/norm, qy/norm, qz/norm, qw/norm
-        R = np.array([
-            [1 - 2*qy*qy - 2*qz*qz, 2*qx*qy - 2*qz*qw, 2*qx*qz + 2*qy*qw],
-            [2*qx*qy + 2*qz*qw, 1 - 2*qx*qx - 2*qz*qz, 2*qy*qz - 2*qx*qw],
-            [2*qx*qz - 2*qy*qw, 2*qy*qz + 2*qx*qw, 1 - 2*qx*qx - 2*qy*qy]
-        ])
-        return R
-    R_A = quaternion_to_rotation_matrix(qx, qy, qz, qw)
-    t_A = np.array([tx, ty, tz])
-    T = np.array([[0, -1, 0], 
-                  [0, 0, -1], 
-                  [1, 0, 0]])
-    R_B = T @ R_A @ T.T
-    t_B = T @ t_A
-    extrinsic = np.eye(4)
-    extrinsic[:3, :3] = R_B
-    extrinsic[:3, 3] = t_B
-    return torch.tensor(extrinsic, dtype=torch.float32)
+def quaternion_to_rotation_matrix(qx, qy, qz, qw):
+    norm = np.sqrt(qx*qx + qy*qy + qz*qz + qw*qw)
+    qx, qy, qz, qw = qx/norm, qy/norm, qz/norm, qw/norm
+    R = np.array([
+        [1 - 2*qy*qy - 2*qz*qz, 2*qx*qy - 2*qz*qw, 2*qx*qz + 2*qy*qw],
+        [2*qx*qy + 2*qz*qw, 1 - 2*qx*qx - 2*qz*qz, 2*qy*qz - 2*qx*qw],
+        [2*qx*qz - 2*qy*qw, 2*qy*qz + 2*qx*qw, 1 - 2*qx*qx - 2*qy*qy]
+    ])
+    return R
 
-def extrinsic_to_tensor(T):
+def quat_to_transform(tx, ty, tz, qx, qy, qz, qw, T_ab):
+    """
+    Convert coordinate system A pose (tx, ty, tz, qx, qy, qz, qw) to transform in B frame.
+    Input:
+    - tx, ty, tz, qx, qy, qz, qw: translation /quaternion in original frame
+    - T_ab: transformation matrix from A to B
+    """
+    assert type(T_ab) == torch.Tensor, "Transformation matrix must be a torch tensor"
+    assert T_ab.shape == (4,4), "Transformation matrix must be 4x4"
+
+    R_A = quaternion_to_rotation_matrix(qx, qy, qz, qw)
+    T_A = torch.eye(4, dtype=T_ab.dtype, device=T_ab.device)
+    T_A[:3, :3] = torch.tensor(R_A, dtype=T_ab.dtype, device=T_ab.device)
+    T_A[:3, 3] = torch.tensor([tx, ty, tz], dtype=T_ab.dtype, device=T_ab.device)
+    T_B = T_ab @ T_A @ torch.inverse(T_ab)
+    return T_B
+
+def transform_to_tensor(T):
+    assert type(T) == torch.Tensor, "Transformation matrix must be a torch tensor"
+    assert T.shape == (4, 4), "Transformation matrix must be 4x4"
+
     t = T[:3, 3]
     R_mat = T[:3, :3]
+    R_mat = R_mat / np.linalg.norm(R_mat, axis=0)
     r = R.from_matrix(R_mat)
     q = r.as_quat()
     return torch.tensor([t[0], t[1], t[2], q[0], q[1], q[2], q[3]])
 
-
-def transform_points(points):
+def transform_points(points, T_ab):
     """
-    Transform points from FAST-LIVO2 to GS world frame.
+    Transform XYZRGB points from frames A to B.
     Points dimension (N, 6), rows are points, columns 0-5 are x, y, z, r, g, b.
-    FAST-LIVO2: x-forward, y-left, z-up
-    GS-LIVO: x-right, y-down, z-forward
+    Input:
+    - points: (N, 6) tensor of points in frame A.
+    - T_ab: (4, 4) transformation matrix tensor from A to B.
     """
+    assert type(points) == torch.Tensor, "Points must be a torch tensor"
+    assert points.shape[1] == 6, "Points must have 6 columns (x, y, z, r, g, b)"
+    assert type(T_ab) == torch.Tensor, "Transformation matrix must be a torch tensor"
+    assert T_ab.shape == (4, 4), "Transformation matrix must be 4x4"
+
     T = torch.eye(6, dtype=torch.float32)
-    T[:3, :3] = torch.tensor([[0, -1, 0],   # y -> x
-                              [0, 0, -1],  # z -> -y
-                              [1, 0, 0]],  # x -> z
-                             dtype=torch.float32)
+    T[:4, :4] = T_ab
     transformed_points = (T @ points.T).T
     return transformed_points
 
@@ -107,15 +112,21 @@ def data_loader(queue, num_frames, dataset_path):
         image_pil = Image.open(f"{dataset_path}/frame{i}/image.png")
         image = torch.tensor(np.array(image_pil)).permute(2, 0, 1).cpu()
 
+        # Define transfrom FAST-LIVO2 to GS coordinate system
+        T = torch.tensor([
+            [ 0, -1,  0, 0],    # -y_fast = x_gs
+            [ 0,  0, -1, 0],    # -z_fast = y_gs
+            [ 1,  0,  0, 0],    # x_fast = z_gs
+            [ 0,  0,  0, 1]   
+        ], dtype=torch.float32)
+        
         tx, ty, tz, qx, qy, qz, qw = torch.load(f"{dataset_path}/frame{i}/pose.pt").tolist()
-        c2w_gs = fastlivo_to_gs_extrinsic(tx, ty, tz, qx, qy, qz, qw)
-        # c2w_gs = torch.inverse(extrinsic)
-        # pose = extrinsic_to_tensor(c2w_gs)
-        w2c_gs = torch.inverse(c2w_gs)
-        pose   = extrinsic_to_tensor(w2c_gs)
+        c2w = quat_to_transform(tx, ty, tz, qx, qy, qz, qw, T)
+        w2c = torch.inverse(c2w)
+        pose   = transform_to_tensor(w2c)
         intrinsics = torch.tensor(np.loadtxt(f"{dataset_path}/intrinsics.txt"))
         lidar_points = torch.tensor(np.load(f"{dataset_path}/frame{i}/points.npy"))
-        transformed_lidar_points = transform_points(lidar_points)
+        transformed_lidar_points = transform_points(lidar_points, T)
 
         batch_data["images"].append(image)
         batch_data["poses"].append(pose)
@@ -149,10 +160,10 @@ if __name__ == '__main__':
     # config_path = "../../config/config_lidar.yaml"
 
     # Elliot testing
-    # num_frames = 500 # normally 1013
-    # dataset_path = "../HI-SLAM2/data/red_sculpture_dense_fixed"
-    num_frames = 78
-    dataset_path = "../HI-SLAM2/data/CBD_Building_01"
+    num_frames = 500 # normally 1013
+    dataset_path = "../HI-SLAM2/data/red_sculpture_dense_fixed"
+    # num_frames = 78
+    # dataset_path = "../HI-SLAM2/data/CBD_Building_01"
     config_path = "../../config/config_lidar.yaml"
 
     queue = mp.Queue(maxsize=8)
