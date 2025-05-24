a3666fbabf83d86b4bf5f5472b9086379083d395
JianwenCao <jianwen__cao@outlook.com>
2025-05-24 19:20:51 +0200

    own keyframe

--- src/3dgs/demo_lidar_VIO.py
+++ src/3dgs/demo_lidar_VIO.py
@@ -1,3 +1,4 @@
+import logging
 import multiprocessing as mp
 import cv2
 import torch
@@ -60,29 +61,87 @@ def transform_points(points, T_ab):
 def data_loader(queue, num_frames, dataset_path, coordinate_transform, intrinsics, K):
     batch_size = 10
     batch_data = {"images": [], "poses": [], "intrinsics": [], "points": [], "tstamp": []}
+    keyframes = {"images": [], "poses": [], "intrinsics": [], "points": [], "tstamp": [], "c2w": []}
+    whole_data = {"images": [], "poses": [], "intrinsics": [], "points": [], "tstamp": [], "w2c": []}
 
     for i in tqdm(range(num_frames), desc="Loading data"):
-        with Image.open(f"{dataset_path}/frame{i}/image.png") as image_pil:
+        try:
+            image_pil = Image.open(f"{dataset_path}/frame{i}/image.png")
             image = np.array(image_pil)
-        if len(intrinsics) > 4:
-            dist_coeffs = np.array(intrinsics[4:])
-            image = cv2.undistort(image, K, dist_coeffs)
-        image = torch.tensor(image).permute(2, 0, 1).cpu()
-        tx, ty, tz, qx, qy, qz, qw = torch.load(f"{dataset_path}/frame{i}/pose.pt").tolist()
-        c2w = quat_to_transform(tx, ty, tz, qx, qy, qz, qw, coordinate_transform)
-        w2c = torch.inverse(c2w)
-        pose = transform_to_tensor(w2c)
+            if len(intrinsics) > 4:
+                dist_coeffs = np.array(intrinsics[4:])
+                image = cv2.undistort(image, K, dist_coeffs)
+            image = torch.tensor(image).permute(2, 0, 1).cpu()
+            tx, ty, tz, qx, qy, qz, qw = torch.load(f"{dataset_path}/frame{i}/pose.pt").tolist()
+            c2w = quat_to_transform(tx, ty, tz, qx, qy, qz, qw, coordinate_transform)
+            w2c = torch.inverse(c2w)
+            pose = transform_to_tensor(w2c)
+            lidar_points = torch.tensor(np.load(f"{dataset_path}/frame{i}/points.npy"))
+            transformed_lidar_points = transform_points(lidar_points, coordinate_transform)
+        except Exception as e:
+            logging.error(e)
 
-        lidar_points = torch.tensor(np.load(f"{dataset_path}/frame{i}/points.npy"))
-        transformed_lidar_points = transform_points(lidar_points, coordinate_transform)
+        is_key = False
+        if i == 0 or i == num_frames - 1:
+            keyframes["images"].append(image)
+            keyframes["poses"].append(pose)
+            keyframes["intrinsics"].append(intrinsics)
+            keyframes["points"].append(transformed_lidar_points)
+            keyframes["tstamp"].append(i)
+            keyframes["c2w"].append(c2w)
+            is_key = True
+        else:
+            last_keyframe_t = keyframes["c2w"][-1][:3, 3]
+            last_keyframe_R = keyframes["c2w"][-1][:3, :3]
+            distance = torch.norm(c2w[:3, 3] - last_keyframe_t)
+            R_rel = torch.matmul(last_keyframe_R.T, c2w[:3, :3])
+            lidar_in_camera = (w2c[:3, :3] @ transformed_lidar_points[:, :3].T + w2c[:3, 3:]).T
+            theta = torch.acos((torch.trace(R_rel) - 1) / 2)
+            ratio = distance / lidar_in_camera[:, -1].mean()
+            if theta > 0.02 or ratio > 0.02:
+                keyframes["images"].append(image)
+                keyframes["poses"].append(pose)
+                keyframes["intrinsics"].append(intrinsics)
+                keyframes["points"].append(transformed_lidar_points)
+                keyframes["tstamp"].append(i)
+                keyframes["c2w"].append(c2w)
+                is_key = True
 
-        batch_data["images"].append(image)
-        batch_data["poses"].append(pose)
-        batch_data["intrinsics"].append(intrinsics)
-        batch_data["points"].append(transformed_lidar_points)
-        batch_data["tstamp"].append(i)
+        whole_data["images"].append(image)
+        whole_data["poses"].append(pose)
+        whole_data["intrinsics"].append(intrinsics)
+        whole_data["points"].append(transformed_lidar_points)
+        whole_data["tstamp"].append(i)
+        whole_data["w2c"].append(w2c)
 
-        if len(batch_data["tstamp"]) == batch_size or i == num_frames - 1:
+        if is_key:
+            print(f"...adding frame {i} to the queue")
+            batch_data["images"].append(image)
+            batch_data["poses"].append(pose)
+            batch_data["intrinsics"].append(intrinsics)
+            batch_data["points"].append(transformed_lidar_points)
+            batch_data["tstamp"].append(i)
+
+        if len(batch_data["tstamp"]) == batch_size or i == (num_frames - 1):
+            if i == (num_frames - 1):
+                print("Number of frames in whole_data:", len(whole_data["tstamp"]))
+                print("Number of frames in keyframes:", len(keyframes["tstamp"]))
+                whole_data["images"] = torch.stack(whole_data["images"])
+                whole_data["poses"] = torch.stack(whole_data["poses"])
+                whole_data["intrinsics"] = torch.stack(whole_data["intrinsics"])
+                whole_data["points"] = whole_data["points"]  # feed in list
+                whole_data["tstamp"] = torch.tensor(whole_data["tstamp"])
+                whole_data["w2c"] = torch.stack(whole_data["w2c"])
+                keyframes["images"] = torch.stack(keyframes["images"])
+                keyframes["poses"] = torch.stack(keyframes["poses"])
+                keyframes["intrinsics"] = torch.stack(keyframes["intrinsics"])
+                keyframes["points"] = keyframes["points"]  # feed in list
+                keyframes["tstamp"] = torch.tensor(keyframes["tstamp"])
+                keyframes["c2w"] = torch.stack(keyframes["c2w"])
+                keyframes['viz_idx'] = keyframes["tstamp"].clone().detach()
+                torch.save(whole_data, "whole_data.pt")
+                torch.save(keyframes, "keyframes.pt")
+
             packet = {
                 'viz_idx': torch.tensor(batch_data["tstamp"]),
                 'tstamp': torch.tensor(batch_data["tstamp"]),
@@ -91,12 +150,17 @@ def data_loader(queue, num_frames, dataset_path, coordinate_transform, intrinsic
                 'points': batch_data["points"],  # feed in list
                 'intrinsics': torch.stack(batch_data["intrinsics"]),
                 'pose_updates': None,
-                'is_last': (i == num_frames - 1)
+                'is_last': (i == num_frames - 1),
             }
-            queue.put(packet)
+            try:
+                queue.put(packet)
+            except Exception as e:
+                logging.error(e)
             batch_data = {"images": [], "poses": [], "intrinsics": [], "points": [], "tstamp": []}
+
     while not queue.empty():
         time.sleep(1)
+        logging.info("Waiting for queue to be consumed")
 
 
 if __name__ == '__main__':
@@ -108,7 +172,7 @@ if __name__ == '__main__':
     )
     parser.add_argument(
         "--dataset", "-d",
-        default="../../dataset/CBD_Building_01_hi_VIO",
+        default="/data/storage/jianwen/CBD_01_VIO",
         help="Path to local dataset w.r.t to the current working directory."
     )
     parser.add_argument(
@@ -118,7 +182,7 @@ if __name__ == '__main__':
     )
     parser.add_argument(
         "--num_frames", "-n", type=int,
-        default=105,
+        default=1181,
         help="Number of frames to process."
     )
     args = parser.parse_args()
@@ -129,7 +193,7 @@ if __name__ == '__main__':
 
     queue = mp.Queue(maxsize=8)
     config = load_config(config_path)
-    gs = GSBackEnd(config, save_dir="./output", use_gui=True)
+    gs = GSBackEnd(config, save_dir="./output", use_gui=False)
 
     coordinate_transform = torch.tensor([
         [1, 0, 0, 0],  # -y_fast = x_gs
@@ -149,26 +213,21 @@ if __name__ == '__main__':
 
     while processed_frames < num_frames:
         packet = queue.get()
-        batch_size = len(packet['tstamp'])
         gs.process_track_data(packet)
-        processed_frames += batch_size
-        pbar.update(batch_size)
+        pbar.update(packet['tstamp'][-1].item() - processed_frames)
+        processed_frames = packet['tstamp'][-1].item()
 
         if packet['is_last']:
+            whole_data = torch.load("whole_data.pt", weights_only=True, map_location="cpu")
+            print("Number of frames in whole_data:", len(whole_data["tstamp"]))
+            keyframes = torch.load("keyframes.pt", weights_only=True, map_location="cpu")
+            print("Number of frames in keyframes:", len(keyframes["tstamp"]))
+            print(keyframes["tstamp"])
+
+            gs.process_track_data(keyframes)
             updated_poses = gs.finalize()
-            gtimages, trajs = [], []
-            for i in range(num_frames):
-                image_pil = Image.open(f"{dataset_path}/frame{i}/image.png")
-                image = np.array(image_pil)
-                if len(intrinsics) > 4:
-                    dist_coeffs = np.array(intrinsics[4:])
-                    image = cv2.undistort(image, K, dist_coeffs)
-                gtimages.append(torch.tensor(image).permute(2, 0, 1).cpu())
-                tx, ty, tz, qx, qy, qz, qw = torch.load(f"{dataset_path}/frame{i}/pose.pt").tolist()
-                extrinsic = torch.inverse(quat_to_transform(tx, ty, tz, qx, qy, qz, qw, coordinate_transform))
-                trajs.append(extrinsic.cuda())
-            gs.eval_rendering({index: tensor for index, tensor in enumerate(gtimages)}, None, trajs,
-                              torch.arange(0, num_frames))
+            
+            gs.eval_rendering(gtimages={index: tensor for index, tensor in enumerate(whole_data["images"].cuda())}, gtdepthdir=None, traj=whole_data["w2c"].cuda(), kf_idx=keyframes["tstamp"])
             break
 
     pbar.close()
